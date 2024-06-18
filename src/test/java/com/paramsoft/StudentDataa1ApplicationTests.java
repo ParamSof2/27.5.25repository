8d269ebf18cc4bf974ba3de7cb80f23b69d64b2c
ParamSof2 <parmeshwarbhargade27@gmail.com>
2024-06-18 19:08:04 +0530

    spring commit

---
+++ src/test/java/com/paramsoft/StudentDataa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paramsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
